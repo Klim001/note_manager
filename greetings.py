2c0c9c2a4462c70ed68444925e00923c20fb5dd6
Klim001 <abramovklim496@gmail.com>
2024-11-26 20:00:36 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Name"
+title = "heading"
+content = "description"
+status = "ожидает, готово, в работе"
+created_date = "xx-xx-xxxx"
+issue_date = "xx-xx-xxxx"
+print("Имя пользователя: ", username)
+print("Заглавие заметки: ", title)
+print("Описание заметки: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", created_date)
+print("Дата истечения заметки: ", issue_date)
